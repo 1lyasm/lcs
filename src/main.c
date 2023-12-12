050950530cd0e11dc427419f6dcc6abe2d3241af
1lyasm <il.mystafa@gmail.com>
2023-12-12 23:01:15 +0300

    add hash

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
+#include <limits.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAX_BUF_LEN 256
+#define N_MODE 2
+#define MAX_LCS_LEN 32
+#define MAX_LCS_BUF_LEN 256
+
+typedef struct {
+    char *str;
+} HashEntry;
 
 typedef struct {
     int **mtx;
@@ -13,6 +22,236 @@ typedef struct {
 void fail(char *msg) {
     printf("%s\n", msg);
     exit(EXIT_FAILURE);
+}
+
+/*
+ @brief Checks if a given number is prime
+
+ @param num Number to be checked
+
+ @return 1 if number is prime, 0 if it is not
+*/
+int checkPrime(int num) {
+    int i;
+    int isPrime = 1;
+    if (num <= 3) {
+        return 1;
+    }
+    for (i = 2; i * i <= num && isPrime == 1; ++i) {
+        if (num % i == 0) {
+            isPrime = 0;
+        }
+    }
+    return isPrime;
+}
+
+/*
+ @brief Computes m value given n and load
+    based on homework specs
+
+ @param n Number of elements
+ @param lf Load factor
+
+ @return m value to be used in hashing
+*/
+int compM(int n, double lf) {
+    int quotient = (int)ceil(n / lf);
+    int m;
+    int isPrime = 0;
+    m = quotient - 1;
+    while (isPrime == 0) {
+        ++m;
+        isPrime = checkPrime(m);
+    }
+    return m;
+}
+
+/*
+ @brief Converts a string to number key using Horner's rule
+
+ @param str String to be converted
+ @param strLen Number of chars in the string
+
+ @return Converted number key for the string
+*/
+int strToNum(char *str, int strLen) {
+    double num = 0;
+    int i;
+    int r = 3;
+    for (i = 0; i < strLen; ++i) {
+        int power = strLen - i - 1;
+        double powerRes = pow(r, power);
+        int charVal;
+        charVal = str[i] - '0' + 1;
+        num = num + powerRes * charVal;
+    }
+    while (num > INT_MAX) {
+        num -= INT_MAX;
+    }
+    return (int)num;
+}
+
+/*
+ @brief Computes output h1 function
+
+ @param key Key to be used in hashing
+ @param m Number of cells in hash table
+
+ @return Output of h1 function
+*/
+int h1(int key, int m) { return key % m; }
+
+/*
+ @brief Computes output h2 function given
+
+ @param key Key to be used in hashing
+ @param m Number of cells in hash table
+
+ @return Output of h2 function
+*/
+int h2(int key, int m) {
+    int m2 = m - 2;
+    return 1 + (key % m2);
+}
+
+/*
+ @brief Computes index in the hash table based on h1 value,
+    h2 value, index i, and m value
+
+ @param h1Val Output of h1 function
+ @param h2Val Output of h2 function
+ @param i Current iteration counter that is used in hashing functions
+ @param m Number of cells in hash table
+
+ @return Computed index in the hash table
+*/
+int compHashIdx(int h1Val, int h2Val, int i, int m) {
+    return (h1Val + i * h2Val) % m;
+}
+
+/*
+ @brief Prints the given hash table
+
+ @param hash Hash table
+ @param m Number of cells in hash table
+
+ @return
+*/
+void printHash(HashEntry *hash, int m) {
+    int i;
+    printf("\nHash table:\n");
+    for (i = 0; i < m; ++i) {
+        if (hash[i].str != 0) {
+            printf("\t%d:\t%s\n", i, hash[i].str);
+        } else {
+            printf("\t%d:\t(null)\n", i);
+        }
+    }
+}
+
+/*
+ @brief Frees the hash table and its substructures
+
+ @param hash Hash table
+ @param m Number of cells in hash table
+
+ @return
+*/
+void freeHash(HashEntry *hash, int m) {
+    int i;
+    for (i = 0; i < m; ++i) {
+        free(hash[i].str);
+    }
+    free(hash);
+}
+
+/*
+ @brief Adds a str to the hash table
+
+ @param hash Hash table
+ @param n Number of original elements
+ @param m Number of cells in hash table
+ @param lf Load factor
+ @param str str to be added
+ @param strLen Length of str to be added
+ @param shouldPrintHash Flag representing whether hash table should
+    be printed after adding
+ @param mode Program running mode
+
+ @return Index of the newly added element, -1 if could not add
+*/
+int add(HashEntry *hash, int m, char *str, int strLen) {
+    int key;
+    int i = 0;
+    int inserted = 0;
+    int hashIdx;
+    int h1Val;
+    int h2Val;
+    int exists = 0;
+    key = strToNum(str, strLen);
+    h1Val = h1(key, m);
+    h2Val = h2(key, m);
+    while (inserted == 0 && exists == 0 && i < m) {
+        hashIdx = compHashIdx(h1Val, h2Val, i, m);
+        if (hash[hashIdx].str == 0) {
+            hash[hashIdx].str = calloc(strLen + 1, sizeof(char));
+            if (hash[hashIdx].str == NULL) {
+                fail("add: malloc failed");
+            }
+            strncpy(hash[hashIdx].str, str, strLen);
+            inserted = 1;
+        } else if (strcmp(hash[hashIdx].str, str) == 0) {
+            exists = 1;
+        }
+        ++i;
+    }
+    if (inserted == 1) {
+    } else if (exists == 1) {
+        hashIdx = -1;
+    } else {
+        hashIdx = -1;
+    }
+    return hashIdx;
+}
+
+/*
+ @brief Takes a str from stdin and finds it in the
+    hash table if possible
+
+ @param hash Hash table
+ @param n Number of original elements
+ @param m Number of cells in hash table
+ @param lf Load factor
+ @param mode Program running mode
+
+ @return
+*/
+int search(HashEntry *hash, int m, char *str, int strLen) {
+    int key;
+    int h1Val;
+    int h2Val;
+    int found = 0;
+    int i = 0;
+    int hashIdx;
+    if (str == NULL) {
+        fail("search: calloc failed");
+        exit(EXIT_FAILURE);
+    }
+    if (strLen >= MAX_LCS_LEN) {
+        str[MAX_LCS_LEN] = 0;
+        strLen = MAX_LCS_LEN;
+    }
+    key = strToNum(str, strLen);
+    h1Val = h1(key, m);
+    h2Val = h2(key, m);
+    while (found == 0 && i < m) {
+        hashIdx = compHashIdx(h1Val, h2Val, i, m);
+        if (hash[hashIdx].str != 0 && strcmp(hash[hashIdx].str, str) == 0) {
+            found = 1;
+        }
+        ++i;
+    }
+    return found;
 }
 
 char *takeStr(char *msg) {
@@ -116,53 +355,72 @@ void printLongestLen(Mtx *mtx) {
 }
 
 void printLcs(Mtx *chosen, char *str1, int len1, char *str2, int len2, int i,
-              int j, char *lcs, int lcsLen) {
-    // printf("printLcs: called with i: %d, j: %d\n", i, j);
+              int j, char *lcs, int lcsLen, HashEntry *hash, int m,
+              int longestLen) {
     if (i == 0 || j == 0) {
-        printf("%s\n", lcs);
+        if (search(hash, m, lcs, lcsLen) == 0) {
+            printf("%s\n", lcs);
+            add(hash, m, lcs, longestLen);
+        }
         return;
     }
     if (chosen->mtx[i][j] == 0) {
         lcs[lcsLen - 1] = str1[j - 1];
-        printLcs(chosen, str1, len1, str2, len2, i - 1, j - 1, lcs, lcsLen - 1);
+        printLcs(chosen, str1, len1, str2, len2, i - 1, j - 1, lcs, lcsLen - 1,
+                 hash, m, longestLen);
     } else if (chosen->mtx[i][j] == 1) {
-        printLcs(chosen, str1, len1, str2, len2, i - 1, j, lcs, lcsLen);
+        printLcs(chosen, str1, len1, str2, len2, i - 1, j, lcs, lcsLen, hash, m,
+                 longestLen);
     } else if (chosen->mtx[i][j] == 2) {
-        printLcs(chosen, str1, len1, str2, len2, i, j - 1, lcs, lcsLen);
+        printLcs(chosen, str1, len1, str2, len2, i, j - 1, lcs, lcsLen, hash, m,
+                 longestLen);
     } else {
-        printLcs(chosen, str1, len1, str2, len2, i - 1, j, lcs, lcsLen);
-        printLcs(chosen, str1, len1, str2, len2, i, j - 1, lcs, lcsLen);
+        printLcs(chosen, str1, len1, str2, len2, i - 1, j, lcs, lcsLen, hash, m,
+                 longestLen);
+        printLcs(chosen, str1, len1, str2, len2, i, j - 1, lcs, lcsLen, hash, m,
+                 longestLen);
     }
 }
 
 int main() {
-    char *str1 = takeStr("Enter string 1: ");
-    char *str2 = takeStr("Enter string 2: ");
-    int len1 = strlen(str1);
-    int len2 = strlen(str2);
-    int m = len1 + 1;
-    int n = len2 + 1;
-    Mtx *mtx = makeMtx(n, m);
-    Mtx *chosen = makeMtx(n, m);
-    int lcsSize = max(len1, len2) + 1;
-    char *lcs = malloc(sizeof(lcsSize * sizeof(char)));
-    if (lcs == NULL) {
-        fail("main: malloc failed");
-    }
-    printf("\nInitial state of matrix: \n\n");
-    printMtx(mtx);
-    fillMtx(mtx, chosen, str1, len1, str2, len2);
-    int longestLen = mtx->mtx[mtx->n - 1][mtx->m - 1];
-    lcs[longestLen] = 0;
-    printf("Length of the longest common subsequence: %d\n\n", longestLen);
-    printf("Longest common subsequences: \n\n");
-    printLcs(chosen, str1, len1, str2, len2, mtx->n - 1, mtx->m - 1, lcs,
-             longestLen);
-    printf("\n");
-    free(lcs);
-    freeMtx(chosen);
-    freeMtx(mtx);
-    free(str1);
-    free(str2);
+    char resp;
+    do {
+        char *str1 = takeStr("\nEnter string 1: ");
+        char *str2 = takeStr("Enter string 2: ");
+        int len1 = strlen(str1);
+        int len2 = strlen(str2);
+        int m = len1 + 1;
+        int n = len2 + 1;
+        Mtx *mtx = makeMtx(n, m);
+        Mtx *chosen = makeMtx(n, m);
+        int lcsSize = max(len1, len2) + 1;
+        int mHash = compM(lcsSize, 1.0);
+        char *lcs = malloc(sizeof(lcsSize * sizeof(char)));
+        if (lcs == NULL) {
+            fail("main: malloc failed");
+        }
+        HashEntry *hash = calloc(mHash, sizeof(HashEntry));
+        if (hash == NULL) {
+            fail("main: calloc failed");
+        }
+        printf("\nInitial state of matrix: \n\n");
+        printMtx(mtx);
+        fillMtx(mtx, chosen, str1, len1, str2, len2);
+        int longestLen = mtx->mtx[mtx->n - 1][mtx->m - 1];
+        lcs[longestLen] = 0;
+        printf("Length of the longest common subsequence: %d\n\n", longestLen);
+        printf("Longest common subsequences: \n\n");
+        printLcs(chosen, str1, len1, str2, len2, mtx->n - 1, mtx->m - 1, lcs,
+                 longestLen, hash, mHash, longestLen);
+        printf("\n");
+        freeHash(hash, mHash);
+        free(lcs);
+        freeMtx(chosen);
+        freeMtx(mtx);
+        free(str1);
+        free(str2);
+        printf("Continue? (enter 'y' for yes): ");
+        scanf(" %c", &resp);
+    } while (resp == 'y');
     return 0;
 }
